4983383eed3abd7a088b8d883b7a90e418d01d62
Piero_Apaza <piero02112003@gmail.com>
2023-11-02 12:40:12 -0500

    Primer commit

---
+++ Business/BInvoice.cs
@@ -0,0 +1,34 @@
+﻿using Data; 
+using Entity;
+using System;
+using System.Collections.Generic;
+
+namespace Business
+{
+    public class BInvoice
+    {
+
+        public List<Invoice> GetInvoicesByDate()
+        {
+            DInvoice dataAccess = new DInvoice();
+            List<Invoice> invoices = dataAccess.Get();
+            return invoices;
+        }
+
+        public bool insert(Invoice invoice)
+        {
+            DInvoice insertar = new DInvoice();
+            insertar.insert(invoice);
+            return true;
+        }
+
+        public bool DeleteRecord(int invoiceID) 
+        {
+            DInvoice delete = new DInvoice();
+
+            return delete.eliminar(invoiceID);
+
+        }
+    }
+}
+    
